502a64f6e7173cd87e7f832d99ab1c07c8e2d8c5
Sonakshi <yadavsonakshi1012@gmail.com>
2025-03-08 21:14:33 +0530

    google.png added

--- frontend/src/components/SignInOAuthButtons.tsx
+++ frontend/src/components/SignInOAuthButtons.tsx
@@ -18,6 +18,7 @@ const SignInOAuthButtons = () => {
     
   return (
     <Button onClick={signInWithGoogle} variant={"secondary"} className="w-full text-white border-zinc-200 h-11">
+      <img src="/google.png" alt="Google" className="size-5" />
       Continue with Google
     </Button>
   )
